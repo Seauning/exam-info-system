9599a8fca54a1f1ab9628d97469284cb78ca21cf
Seauning <1648449524@qq.com>
2023-04-19 21:15:52 +0800

    feat: 表格字段更改

--- src/views/FilterTable/index.tsx
+++ src/views/FilterTable/index.tsx
@@ -5,28 +5,77 @@ import css from './index.module.scss'
 import { useColumns } from './useColumns'
 
 export interface SchoolItem {
-  no: string;
-  major?: string;
-  enrollmentUniversitie?: string;
-  educationalInstitution?: string;
-  lianpeiSchool?: string;
-  enrollmentPlan?: string;
-  normalPlan?: string;
-  povertyAlleviationFamiliy?: string;
-  awardStudent?: string;
-  futureCraftsman?: string;
-  retiredSoldiers?: string;
-  exam300?: string;
-  exam150?: string;
-  tuition?: string;
-  provincialControlLine?: string;
-  admissionScoreLineForOrdinaryCandidate?: string;
-  admissionScorePovertyAlleviationFamilies?: string;
-  admissionScoreLineAwardStudents?: string;
-  provincialControlLineBranchOrdinaryCandidate?: string;
-  vacancyPlanRegularCandidate?: string;
-  vacancyPlanPovertyAlleviationFamiliy?: string;
-  vacancyProgramAwardStudents?: string;
+  /** 本科院校代码 */
+  schoolCode: string;
+
+  /** 招生高校 */
+  schoolName: string;
+
+  /** 院校性质 */
+  schoolType: string;
+
+  /** 联培学校 */
+  coSchool: string;
+
+  /** 本科专业代码 */
+  learnCode: string;
+
+  /** 本科专业 */
+  learn: string;
+
+  /** 招生计划 */
+  needNum: string;
+
+  /** 普通计划 */
+  ptNum: string;
+
+  /** 脱贫计划 */
+  tpNum: string;
+
+  /** 获奖学生 */
+  hjNum: string;
+
+  /** 未来工匠 */
+  gjNum: string;
+
+  /** 退役士兵 */
+  tyNum: string;
+
+  /** 考试科目 */
+  test: string;
+
+  /** 学费 */
+  cost: string;
+
+  /** 省控线 */
+  skx: string;
+
+  /** 退役缺额 */
+  tyqe: string;
+
+  /** 工匠缺额 */
+  gjqe: string;
+
+  /** 脱贫缺额 */
+  tpqe: string;
+
+  /** 获奖缺额 */
+  hjqe: string;
+
+  /** 普通缺额 */
+  ptqr: string;
+
+  /** 工匠录取分数 */
+  gjScore: string;
+
+  /** 脱贫录取分数 */
+  tpScore: string;
+
+  /** 获奖录取分数 */
+  hjScore: string;
+
+  /** 普通分数线 */
+  ptScore: string;
 }
 
 export type FilterTableProps = {
